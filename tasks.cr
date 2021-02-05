110a941e1627b0cbdc5df9775dd1b281897f5acc
Jeremy Woertink <jeremywoertink@gmail.com>
2021-02-05 15:41:08 -0800

    Release 0.7.2 (#54)
    * release 0.7.2
    
    * Avram now comes built with a db.reset task. No need for this one

--- tasks.cr
+++ tasks.cr
@@ -3,18 +3,4 @@ require "./src/authentic"
 require "./config"
 require "./db/migrations/*"
 
-class Db::Reset < LuckyCli::Task
-  summary "Drop, creates and migrates the database"
-
-  def call
-    run("lucky db.drop")
-    run("lucky db.create")
-    run("lucky db.migrate")
-  end
-
-  private def run(command)
-    Process.run(command, shell: true, output: STDOUT, error: STDERR)
-  end
-end
-
 LuckyCli::Runner.run
