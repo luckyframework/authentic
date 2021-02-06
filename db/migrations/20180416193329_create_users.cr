8b2b082750d80d2b77135fd88c04c61a04649640
Rémy Marronnier <rmarronnier@gmail.com>
2021-02-06 23:52:21 +0100

    Updating migration syntax to use @matthewmcgarvey  upstream work magic

--- db/migrations/20180416193329_create_users.cr
+++ db/migrations/20180416193329_create_users.cr
@@ -1,15 +1,10 @@
 class CreateUsers::V20180416193329 < Avram::Migrator::Migration::V1
   def migrate
+    enable_extension "citext"
     create :users do
-      add email : String, unique: true
+      add email : String, unique: true, case_sensitive: false
       add encrypted_password : String
     end
-    execute <<-SQL
-    CREATE EXTENSION IF NOT EXISTS citext;
-SQL
-    execute <<-SQL
-    ALTER TABLE users ALTER COLUMN email TYPE citext;
-SQL
   end
 
   def rollback
