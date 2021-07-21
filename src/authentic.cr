76e76aabb7d8f5d5145aeaba45ea91a213411115
Jeremy Woertink <jeremywoertink@gmail.com>
2021-07-21 10:50:47 -0700

    Move the secret_key validation to Habitat.  (#65)
    * Move the secret_key validation to Habitat. Remove the old secret_key method which seemed to be broken anyway. Fixes #61
    
    * Just removing this method since it never would have compiled before anyway

--- src/authentic.cr
+++ src/authentic.cr
@@ -34,7 +34,22 @@ module Authentic
   Habitat.create do
     setting encryption_cost : Int32 = Crypto::Bcrypt::DEFAULT_COST
     setting default_password_reset_time_limit : Time::Span = 15.minutes
-    setting secret_key : String
+    setting secret_key : String, validation: :validate_length
+  end
+
+  def self.validate_length(value : String)
+    if value.bytesize != 32
+      Habitat.raise_validation_error <<-ERROR
+
+      Authentic secret_key setting must be 32 bytes long.
+
+      Try this...
+
+        ▸ Generate a new key with `lucky gen.secret_key`
+        ▸ Set the secret_key value in your Authentic.configure block
+
+      ERROR
+    end
   end
 
   # Remember the originally requested path if it is a GET
@@ -142,12 +157,5 @@ module Authentic
     encryptor = Lucky::MessageEncryptor.new(secret: settings.secret_key)
     user_id, expiration_in_ms = String.new(encryptor.verify_and_decrypt(token)).split(":")
     Time.utc.to_unix_ms <= expiration_in_ms.to_i64 && user_id.to_s == authenticatable.id.to_s
-  end
-
-  private def self.secret_key
-    if settings.secret_key.length != 32
-      raise "Authentic secret_key must be 32 characters long"
-    end
-    settings.secret_key
   end
 end
